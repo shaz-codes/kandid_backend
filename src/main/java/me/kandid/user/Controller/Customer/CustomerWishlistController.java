786f3f3adf2dab9bff693b335a32eb4fb894c7df
shaz-codes <shahnaz_BE23CS153@srmcem.ac.in>
2025-08-25 14:33:07 +0530

    fix: added cors

--- src/main/java/me/kandid/user/Controller/Customer/CustomerWishlistController.java
+++ src/main/java/me/kandid/user/Controller/Customer/CustomerWishlistController.java
@@ -15,7 +15,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "https://kandid.me")
 @RequestMapping("wishlist")
 @Tag(
         name = "Customer Wishlist",
